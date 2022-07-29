74b45ed924974bb8699cb4aaa1567dbcc7d5f55f
zakb27 <zakbrook02@gmail.com>
2022-07-29 17:50:48 +0100

    Finished styling and fixed more bug fixes

--- public/script.js
+++ public/script.js
@@ -1,36 +1,69 @@
-
+let currentPosition;
 const replacer = (item)=>{
-    document.querySelector(".feels").textContent=item.main.feels_like;
-    document.querySelector(".humidity").textContent=item.main.humidity;
-    document.querySelector(".pressure").textContent=item.main.pressure;
-    document.querySelector(".min").textContent=item.main.temp_min;
-    document.querySelector(".max").textContent=item.main.temp_max;
-    document.querySelector('.temp').textContent=item.main.temp;
-    document.querySelector('.temp').textContent=item.main.temp;
+    let current =Math.round (Number(item.main.feels_like)-273);
+    document.querySelector(".feels").textContent=current.toString() + '°';
+    document.querySelector(".humidity").textContent=item.main.humidity + '%';
+    document.querySelector(".pressure").textContent=item.main.pressure + 'mb';
+    current =Math.round (Number(item.main.temp_min)-273);
+    document.querySelector(".min").textContent=current.toString() + '°';
+    current = Math.round(Number(item.main.temp_max)-273);
+    document.querySelector(".max").textContent=current.toString()+ '°';
+    current = Math.round(Number(item.main.temp)-273);
+    document.querySelector('.temp').textContent=current.toString()+ '°';
     document.querySelector('.smaller_location').textContent=item.name;
     document.querySelector('.weather').textContent=item.weather[0].main;
     document.querySelector('.weather_small').textContent=item.weather[0].description;
 }
 
+const empty = async()=>{
+    document.querySelector(".feels").textContent='';
+    document.querySelector(".humidity").textContent='';
+    document.querySelector(".pressure").textContent='';
+    document.querySelector(".min").textContent='';
+    document.querySelector(".max").textContent='';
+    document.querySelector('.temp').textContent='';
+    document.querySelector('.smaller_location').textContent='';
+    document.querySelector('.weather').textContent='';
+    document.querySelector('.weather_small').textContent='';
+}
 
 
-async function test(item='London'){
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q='+item+'&APPID='+token);
-    console.log(item);
-    const info = await response.json();
-    console.log(info);
-    replacer(info);
+async function test(item){
+    try {
+        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + item + '&APPID=' + token);
+        console.log(item);
+        const info = await response.json();
+        console.log(info);
+        replacer(info);
+    }
+    catch(error){
+        await empty();
+        alert('Cannot input false location');
+    }
+}
+
+async function currentData(item){
+    try {
+        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + item.lat + '&lon=' + item.lng + '&APPID=' + token);
+        const info = await response.json();
+        console.log(info);
+        replacer(info);
+    }
+    catch(error){
+        await empty();
+        alert('Location data error try again');
+    }
 }
 
 const switchDisplay=()=>{
 
     if(value2.style.display==='none'){
-        value2.style.display='block'
+        value2.style.display='flex'
         value.style.display='none';
     }
     else{
         value2.style.display = 'none';
-        value.style.display='block';
+        value.style.display='flex';
     }
 }
 
@@ -38,13 +71,13 @@ const switchDisplay=()=>{
 
 async function mapDirection(){
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPos);
+        navigator.geolocation.getCurrentPosition(await showPos);
     } else{
         alert("Sorry, browser does not support geolocation!");
     }
 }
 async function showPos(position){
-    currentPosition = {"lat":position.coords.latitude,"lng":position.coords.longitude};
+    currentPosition = {lat:position.coords.latitude,lng:position.coords.longitude};
 }
 
 
@@ -55,21 +88,32 @@ const value2 = document.querySelector('.display');
 const returns = document.querySelector('#switcher');
 const current = document.querySelector('#current_location');
 value2.style.display='none';
+mapDirection();
+
+
+
 
 form.addEventListener('submit',(event) =>{
-    switchDisplay();
+
+
     test(second.value);
     event.preventDefault();
+    switchDisplay();
 });
+
+
 
 returns.addEventListener('click',() =>{
-    switchDisplay();
+
     second.value='';
+    switchDisplay();
 });
 
+
 current.addEventListener('click',async() =>{
-    switchDisplay();
+
     await mapDirection();
     console.log(currentPosition);
-    test(currentPosition);
+    await currentData(currentPosition);
+    switchDisplay();
 });
